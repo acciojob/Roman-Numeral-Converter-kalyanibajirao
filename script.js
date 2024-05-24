7b4f413d439d40cba1aab443cfce31a8d60a1f9a
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-05-24 13:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
 
-  //your code here
 	onst obj = {
         0: ['M', 1000],
         1: ['D', 500],
